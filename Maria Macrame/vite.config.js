d155ebefe16c5ba0ab27e8f6f2c73fd58d204c9a
Rafaela <rafaela.vaz.quaresma@students.beaminstitute.org>
2024-06-22 09:47:11 +0200

    Fix asset paths for GitHub Pages deployment

--- Maria Macrame/vite.config.js
+++ Maria Macrame/vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/e-commerce-React-final-project/', // Ensure this matches your repository name
   plugins: [react()],
   resolve: {
     alias: {
